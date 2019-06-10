94b1cb5f3a6c8d368bddc5cd09579898a2b49ed7
Lauren Gottberg <lauren.gottberg@pendo.io>
2019-06-10 15:05:27 -0400

    uploading sample app

---
+++ js/initPendo.js
@@ -0,0 +1,9 @@
+/*
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+        v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
+            o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+})('9cc10e9d-ab4c-48a4-7561-57cd8c3d836b');
+*/
