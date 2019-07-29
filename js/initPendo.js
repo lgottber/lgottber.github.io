963cf3382577c734841c3cf81688e12bbc4fd211
Lauren Gottberg <lauren.gottberg@pendo.io>
2019-07-29 15:43:45 -0400

    updating key

--- js/initPendo.js
+++ js/initPendo.js
@@ -4,4 +4,4 @@
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://pendo-atlas-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-})('539ca0de-b75b-4a12-44fe-9282569f3728');
+})('1890da7e-c2ad-4295-633d-11f5453c95a5');
